1d6b222c7e2b8bbde2e1d1f0fc16684f168c14ed
yenat <yenatshif@gmail.com>
2021-01-12 13:00:26 +0300

    Auto-commit

--- git_push.sh
+++ git_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-LOCAL_REPO="/home/bitseat/visualizer"
-USERNAME="yenatshif@gmail.com"
+LOCAL_REPO="/home/bitseat/visualizer/"
+USERNAME="yenat"
 PASSWORD="103671cbfb177cfeef70d220a62a915cb375d80a"
 REMOTE_REPO="github.com/yenat/visualizer.git"
 EMAIL="yenatshif@gmail.com"
@@ -9,25 +9,11 @@ EMAIL="yenatshif@gmail.com"
 cd $LOCAL_REPO
 
 
-add_commit_push()
-{
-    echo "-------ADD COMMENTS-------" 
-    git add cia_new_tensor.tsv cia_new_metadata.tsv 
-    echo "-------COMMIT COMMENTS-------" 
-    git commit -m "Auto-commit"
-    echo "-------PUSH COMMENTS-------" 
+echo "-------ADD COMMENTS-------" 
+git add *
+echo "-------COMMIT COMMENTS-------" 
+git commit -m "Auto-commit"
+echo "-------PUSH COMMENTS-------" 
 
-    sudo git push -u https://yenat:103671cbfb177cfeef70d220a62a915cb375d80a@$REMOTE_REPO master 
-
-
-
-    if [[ $? != 0 ]]; then
-
-        echo "push failed"
-        exit 1
-    fi
-    echo "success!"
-}
-
-add_commit_push
+git push -u https://yenat:103671cbfb177cfeef70d220a62a915cb375d80a@$REMOTE_REPO master 
 
